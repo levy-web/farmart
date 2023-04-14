c6ce80976c57cacace4c57e05bf5c6f402cbc6f8
dancunrandy <dancunrandy@gmail.com>
2023-04-14 22:49:56 +0300

    home page

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import { Navbar, Footer } from "../components";
 
-const Home = () => {
+function Home() {
   return (
-    <div>
-    <Navbar/>
-    </div>
+    <>
+      <Navbar />
+      <Footer />
+    </>
   )
 }
 
