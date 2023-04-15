11bd34c845f039d67c3c7aeea85a9844934428e4
dancunrandy <dancunrandy@gmail.com>
2023-04-15 17:21:24 +0300

    landing page

---
+++ frontend/src/components/main.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="hero border-1 pb-3">
+        <div className="card bg-dark text-white border-0 mx-3">
+          <img
+            className="card-img img-fluid"
+            src="./assets/coder.jpg"
+            alt="Card"
+            height={1000}
+          />
+          <div className="card-img-overlay d-flex align-items-center">
+            <div className="container" style={{position:'absolute',top:0,left:0}}>
+              <h5 className="card-title fs-1 text fw-bold" style={{color:'black'}}>Welcome to Farmat</h5>
+              <p className="card-text fs-5 d-none d-sm-block " style={{color:'black'}}>
+              Buy farm animals directly from farmers and support their livelihoods
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
