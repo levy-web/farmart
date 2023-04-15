a73a3b9c47d3677deeec48ea40c5e863c8aaca89
dancunrandy <dancunrandy@gmail.com>
2023-04-15 17:11:13 +0300

    added link

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                             <NavLink className="nav-link" to="/">Home </NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link" to="/product">Animals</NavLink>
+                            <NavLink className="nav-link" to="/Animal">Animals</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/about">About</NavLink>
