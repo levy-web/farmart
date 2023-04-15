0cc0538f993187dbcd4baeea4ef52555944e6f2e
dancunrandy <dancunrandy@gmail.com>
2023-04-15 17:02:09 +0300

    fetch function

---
+++ frontend/src/components/Animals.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addCart } from "../redux/action";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+import { Link } from "react-router-dom";
+
+const Animals = () => {
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState(data);
+  const [loading, setLoading] = useState(false);
+  let componentMounted = true;
+
+  const dispatch = useDispatch();
+
+  const addAnimal = (animal) => {
+    dispatch(addCart(animal))
+  }
+
+  useEffect(() => {
+    const getAnimals = async () => {
+      setLoading(true);
+      const response = await fetch("");
+      if (componentMounted) {
+        setData(await response.clone().json());
+        setFilter(await response.json());
+        setLoading(false);
+      }
+
+      return () => {
+        componentMounted = false;
+      };
+    };
