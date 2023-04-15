4eb200c31c217364fc4c3d979a5f42db763ad3b3
dancunrandy <dancunrandy@gmail.com>
2023-04-15 16:57:21 +0300

    added logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,10 +7,16 @@ const Footer = () => {
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
-              <a  href=" /" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">dancunrandy</a>
+              <a  href="https://dancunrandy.vercel.app" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">dancunrandy</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/dancunrandy" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
+            </a>
+            <a className="text-dark fs-4" href="https://github.com/dancunrandy" target="_blank" rel="noreferrer">
+              <i className="fa fa-facebook"></i>
+            </a>
+            <a className="text-dark fs-4" href="https://github.com/dancunrandy" target="_blank" rel="noreferrer">
+              <i className="fa fa-instagram"></i>
             </a>
           </div>
         </div>
