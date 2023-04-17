1a37880545698d36f6b3535ede241e7612fb2a11
kennedy <kenneyrit@gmail.com>
2023-04-17 10:13:13 +0300

    validated the user model

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-    has_secure_password
-
+    # has_secure_password
     has_many :orders
     has_many :reviews
-    has_many :carts
 
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
     validates :username, presence: true, uniqueness: true
-    validates :password, length: {minimum: 6}
+    validates :password, length: {minimum: 6}, if: -> { new_record? || !password.nil?}
+
 end
