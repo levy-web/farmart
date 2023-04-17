04deaa9b58b04b3c0d6a05c1c18f9e7d84c20be6
matomugo60 <matomugo60@gmail.com>
2023-04-17 14:59:25 +0300

    provided code for carts_controller.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :carts
+  resources :carts, only: [:index, :show, :create, :update, :destroy]
   resources :line_items, only: [:create]
   resources :orders
   resources :reviews
