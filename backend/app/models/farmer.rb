32571a37bd3c54cda74e077dd3fb5e4ee1c1b87e
kennedy <kenneyrit@gmail.com>
2023-04-17 10:14:02 +0300

    validared the farmer

--- backend/app/models/farmer.rb
+++ backend/app/models/farmer.rb
@@ -1,12 +1,10 @@
 class Farmer < ApplicationRecord
-    has_secure_password
-    
     has_many :animals
     has_many :orders
 
     validates :contact_info, {presence: true, uniqueness: true, length: {minimum: 10, maximum:10}}
     validates :email, presence: true, uniqueness: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
-    validates :admin_name, presence: true, uniqueness: true
-    validates :password, length: {minimum: 6}
+    validates :username, presence: true, uniqueness: true
+    validates :password, length: {minimum: 6}, if: -> { new_record? || !password.nil?}
 end
