31340ce90ce5ed48920fb472094efa69c044f46c
levy <lnjiru43@gmail.com>
2023-04-18 11:51:33 +0300

    rendered component routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import FarmerNav from './components/farmer/FarmerNav';
+import MyAnimals from './components/farmer/myAnimals/MyAnimals';
+import AnimalUpdator from './components/farmer/myAnimals/AnimalUpdator';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './components/redux/Store';
+import AddSell from './components/farmer/AddSell';
+import Orders from './components/farmer/Orders/Orders';
+import Profile from './components/farmer/Profile';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store = {store}>
+      <FarmerNav />
+      <Routes>
+        <Route path={'/for_sale'} element={<MyAnimals/>} ></Route>
+        <Route path={'/add_for_sale'} element={<AddSell/>} ></Route>
+        <Route path={'/orders'} element={<Orders/>} ></Route>
+        <Route path={'/profile'} element={<Profile />} ></Route>
+        <Route path={'/animals/:animalsName'} element={<AnimalUpdator/>} ></Route>
+      </Routes>
+    </Provider>
   );
 }
 
