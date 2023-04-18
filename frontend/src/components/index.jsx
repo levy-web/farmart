7c4318fcadc7b52a997b54b415a330deaeba1d2d
dancunrandy <dancunrandy@gmail.com>
2023-04-18 09:34:40 +0300

    fixed errors

---
+++ frontend/src/components/index.jsx
@@ -0,0 +1,4 @@
+export { default as Navbar } from './Navbar';
+export { default as Main } from './main';
+export { default as Animal } from './Animals';
+export { default as Footer } from './Footer';
--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Footer, Navbar } from "../components";
+import {Footer, Navbar} from '../components'
 const AboutPage = () => {
   return (
     <>
