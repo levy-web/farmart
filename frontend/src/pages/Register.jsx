85bf2457b9fa25d9b45c3dfd9923515f6e33f1a9
matomugo60 <matomugo60@gmail.com>
2023-04-20 11:07:24 +0300

    provided code for register functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Footer, Navbar } from "../components";
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleEmailChange = (e) => setEmail(e.target.value);
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ name, email, password })
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create user account');
+      }
+
+      setIsLoading(false);
+      // redirect user to login page
+      window.location.href = '/login';
+    } catch (error) {
+      setIsLoading(false);
+      setErrorMessage(error.message);
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -11,7 +47,10 @@ const Register = () => {
           <div className="col-md-7 col-lg-7 col-sm-8 mx-auto">
             <div className="card shadow p-5">
               <h1 className="text-center mb-5">Join farmart</h1>
-              <form>
+              {errorMessage && (
+                <div className="alert alert-danger">{errorMessage}</div>
+              )}
+              <form onSubmit={handleSubmit}>
                 <div className="form-group mb-3">
                   <input
                     type="text"
@@ -19,6 +58,8 @@ const Register = () => {
                     id="name"
                     placeholder="Full Name"
                     required
+                    value={name}
+                    onChange={handleNameChange}
                   />
                 </div>
                 <div className="form-group mb-3">
@@ -28,6 +69,8 @@ const Register = () => {
                     id="email"
                     placeholder="Email"
                     required
+                    value={email}
+                    onChange={handleEmailChange}
                   />
                 </div>
                 <div className="form-group mb-3">
@@ -37,15 +80,17 @@ const Register = () => {
                     id="password"
                     placeholder="Password"
                     required
+                    value={password}
+                    onChange={handlePasswordChange}
                   />
                 </div>
                 <div className="text-center mb-4">
                   <button
                     type="submit"
                     className="btn btn-primary btn-block w-100"
-                    disabled
+                    disabled={isLoading}
                   >
-                    Register
+                    {isLoading ? 'Registering...' : 'Register'}
                   </button>
                 </div>
               </form>
@@ -63,18 +108,18 @@ const Register = () => {
                 </p>
                 <p>
                   Have an account?{' '}
-                  <Link to="/login" className="text-info">
-                    Login
-                  </Link>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
-};
+                  <Link to="/login"className="text-info">
+                                  Login
+                              </Link>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                  </div>
+                      <Footer />
+                                </>
+              );
+              };
 
 export default Register;
