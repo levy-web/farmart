6c9203900add75899b674cccc2744536d247c8ad
levy <lnjiru43@gmail.com>
2023-04-21 10:14:39 +0300

    added a function to attach an image

--- backend/app/models/animal.rb
+++ backend/app/models/animal.rb
@@ -2,4 +2,11 @@ class Animal < ApplicationRecord
     belongs_to :farmer
     has_many :line_items
     has_many :reviews
+    has_one_attached :image
+
+    validates :name, presence: true, uniqueness: true
+
+    def image_url 
+        Rails.application.routes.url_helpers.url_for(image) if image.attached?        
+    end
 end
