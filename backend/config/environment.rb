21d92247f9d10a18a40efe3307c20a7389507622
levy <lnjiru43@gmail.com>
2023-04-21 12:46:59 +0300

    preoduction env

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -3,8 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.application.configure do
-    config.secret_key_base = Rails.application.credentials.production[:secret_key_base]
-    # Other production configuration settings
-end
