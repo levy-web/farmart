f76a50b827645a569e665684542f0f0d8d9a54ff
levy <lnjiru43@gmail.com>
2023-04-21 12:19:30 +0300

    render deploy

--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+    config.secret_key_base = Rails.application.credentials.production[:secret_key_base]
+    # Other production configuration settings
+end
