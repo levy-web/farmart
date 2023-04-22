52da568df269d20f6c91e75e0ca763aa68c6f0b7
matomugo60 <matomugo60@gmail.com>
2023-04-22 14:33:17 +0300

    updated contact page to use emailjs

--- frontend/src/pages/ContactPage.jsx
+++ frontend/src/pages/ContactPage.jsx
@@ -1,50 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Navbar } from "../components";
+import emailjs from 'emailjs-com';
+
 const ContactPage = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_5lxs01n', 'template_iwd41x6', e.target, 'FsFo6NjUnXqn1M2EQ')
+      .then(() => {
+          setName("");
+          setEmail("");
+          setMessage("");
+          setShowSuccessMessage(true);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <>
       <Navbar />
       <div className="container my-3 py-3">
         <h1 className="text-center">Contact Us</h1>
         <hr />
-        <div class="row my-4 h-100">
+        <div className="row my-4 h-100">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-            <form>
-              <div class="form my-3">
-                <label for="Name">Name</label>
+            <form onSubmit={handleSubmit}>
+              <div className="form my-3">
+                <label htmlFor="name">Name</label>
                 <input
-                  type="email"
-                  class="form-control"
-                  id="Name"
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  name="name"
                   placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  required
                 />
               </div>
-              <div class="form my-3">
-                <label for="Email">Email</label>
+              <div className="form my-3">
+                <label htmlFor="email">Email</label>
                 <input
                   type="email"
-                  class="form-control"
-                  id="Email"
+                  className="form-control"
+                  id="email"
+                  name="email"
                   placeholder="name@example.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
                 />
               </div>
-              <div class="form  my-3">
-                <label for="Password">Message</label>
+              <div className="form my-3">
+                <label htmlFor="message">Message</label>
                 <textarea
                   rows={4}
-                  class="form-control"
-                  id="Password"
+                  className="form-control"
+                  id="message"
+                  name="message"
                   placeholder="Enter your message"
+                  value={message}
+                  onChange={(e) => setMessage(e.target.value)}
+                  required
                 />
               </div>
               <div className="text-center">
-                <button
-                  class="my-2 px-4 mx-auto btn btn-dark"
-                  type="submit"
-                  disabled
-                >
-                  Send
-                </button>
+                {showSuccessMessage ? (
+                  <p className="text-success">Thank you for your feedback!</p>
+                ) : (
+                  <button
+                    className="my-2 px-4 mx-auto btn btn-dark"
+                    type="submit"
+                  >
+                    Send
+                  </button>
+                )}
               </div>
             </form>
           </div>
@@ -56,3 +91,7 @@ const ContactPage = () => {
 };
 
 export default ContactPage;
+
+
+
+// SERVICE_ID ('service_5lxs01n', TEMPLATE_ID 'template_iwd41x6', PUBLIC_KEY 'FsFo6NjUnXqn1M2EQ')
