0d614cb8eb091a293c6549d6b9bcbb9d13121fa6
levy <lnjiru43@gmail.com>
2023-04-23 22:40:53 +0300

    redux farmer action , type and slice

---
+++ frontend/src/components/redux/farmer/FarmerAction.js
@@ -0,0 +1,47 @@
+import { REMOVE_FARMER, LOGIN_FARMER, LOAD_FARMER, LOGIN_FARMER_ERROR } from "./FarmerType";
+
+
+export function loginFarmer(email, password) {
+  
+    return async function (dispatch) {
+      
+      dispatch({ type: LOAD_FARMER });
+
+      try {
+
+        const response = await fetch(`http://localhost:3000/farmer-login`,{
+        method:"POST",
+        headers:{"Content-Type":"application/json"},
+        body:JSON.stringify({
+          "email":email,
+          "password":password
+          })
+        });
+        if (!response.ok) {
+          
+          dispatch({
+            type: LOGIN_FARMER_ERROR,
+            payload: 'Network response was not ok'
+          })
+        }
+        const data = await response.json();
+        if (data.status !== "ok") {
+          dispatch({
+            type: LOGIN_FARMER_ERROR,
+            payload: data.data.error
+          })
+          
+        }else{
+          dispatch({
+            type: LOGIN_FARMER,
+            payload: data
+          })
+          
+        }
+
+      }catch (error){
+        console.error(error)
+      }
+      
+    };
+}
