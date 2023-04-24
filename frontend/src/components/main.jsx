bba948e649111bc3ca23b2688d366ef502e8b5a6
matomugo60 <matomugo60@gmail.com>
2023-04-24 11:54:53 +0300

    changed styling in main.jsx

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -11,14 +11,17 @@ const Home = () => {
             alt="Card"
             height={1000}
           />
-          <div className="card-img-overlay d-flex align-items-center">
-            <div className="container mt-3" style={{position:'absolute',top:0,left:0}}>
-            <h5 className="card-title fs-1 text fw-bold" style={{ color: 'black', textAlign: 'center', margin: 3 }}>Welcome to Farmat</h5>
-                <p className="card-text fs-5 d-none d-sm-block " style={{ color: 'black', textAlign: 'center', margin: 3 }}>
-                  Buy farm animals directly from farmers and support their livelihoods
-                </p>
-            </div>
-          </div>
+      <div className="card-img-overlay d-flex align-items-start">
+  <div className="container mt-1">
+    <h5 className="card-title fs-1 text-center fw-bold" style={{ color: 'black', fontWeight: 'bold', margin: '3rem 0 1rem' }}>
+      Welcome to Farmat
+    </h5>
+    <p className="card-text fs-5 text-center" style={{ color: 'black', fontWeight: 'bold', margin: '1rem 0 3rem' }}>
+      Buy farm animals directly from farmers and support their livelihoods
+    </p>
+  </div>
+</div>
+
         </div>
       </div>
     </>
