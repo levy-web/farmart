260d0bdab9dc446168aa080919d00719e7f5cef5
kennedy <kenneyrit@gmail.com>
2023-04-24 12:43:32 +0300

    updated carosel captions

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -13,8 +13,8 @@ const Home = () => {
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h3>Animals for sale</h3>
+          <p>Dairy cows for sale.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -25,8 +25,8 @@ const Home = () => {
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h3>Animals for sale</h3>
+          <p>Dairy goat for sale.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
@@ -37,9 +37,9 @@ const Home = () => {
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
+          <h3>Animals for sale </h3>
           <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            Wooly sheep for sale.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
