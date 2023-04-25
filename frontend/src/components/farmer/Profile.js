0d0d10dd7a8bbbc7e0bf53918b9f9c7aa89647c7
matomugo60 <matomugo60@gmail.com>
2023-04-25 12:22:25 +0300

    updated styling in profile.js

--- frontend/src/components/farmer/Profile.js
+++ frontend/src/components/farmer/Profile.js
@@ -1,33 +1,42 @@
+
 import React, { useEffect, useState } from 'react'
 import FarmerNav from './FarmerNav'
-
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap'
 function Profile() {
-    const [myProfile, setMyProfile] = useState([])
-
-    useEffect(()=>{
-        fetch('/farmer/:id')
-        .then((r)=>r.json())
-        .then((data)=>{
-            setMyProfile(data)
-            console.log(data)
-        })
+const [myProfile, setMyProfile] = useState([])
+useEffect(()=>{
+    fetch('/farmer/:id')
+    .then((r)=>r.json())
+    .then((data)=>{
+        setMyProfile(data)
+        console.log(data)
     })
-
-  return (
+})
+return (
     <>
     <FarmerNav />
     <div className='container'>
-        <h3>Farmer Details</h3>
-        <br/>
-        <div className='m-auto'>
-            <img src="" alt="pic" />
-            <h2>John Doe</h2>
-            <p>Best farm producer seller in town</p>
-            <a href="farmer.com">"farmer.com"</a>
-        </div>      
+    <h3 className='text-center'>Farmer Details</h3>
+    <br/>
+    <div className='d-flex justify-content-center align-items-center'>
+    <img src="https://via.placeholder.com/150" alt="Profile avatar" className='rounded-circle me-3' />
+    <div>
+    <h2>John Doe</h2>
+    <p className='fw-bold'>Bio: Best farm animal seller in town</p>
+    <a href="/" className='text-muted'>farmart.com</a>
+    </div>
+    </div>
+    <br/>
+    <Card className='w-75 m-auto'>
+    <ListGroup>
+    <ListGroupItem>Location: {myProfile.location}</ListGroupItem>
+    {/* <ListGroupItem>Experience: {myProfile.experience}</ListGroupItem> */}
+    <ListGroupItem>Contact: {myProfile.contact}</ListGroupItem>
+    {/* <ListGroupItem>Description: {myProfile.description}</ListGroupItem> */}
+    </ListGroup>
+    </Card>
     </div>
     </>
-  )
-}
-
-export default Profile
+    )
+    }
+    export default Profile
