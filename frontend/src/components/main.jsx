76229e45056e2f73b8e4407a84b8e6992fb659d1
matomugo60 <matomugo60@gmail.com>
2023-04-25 09:28:21 +0300

    updated images in main.jsx

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -9,37 +9,37 @@ const Home = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./assets/coder.jpg"
+          src="https://images.unsplash.com/photo-1614183653806-b4b9daed1954?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGZhcm0lMjBhbmltYWxzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
           alt="First slide"
         />
         <Carousel.Caption>
           <h3>Animals for sale</h3>
-          <p>Dairy cows for sale.</p>
+          <p>Woolly Sheep for Sale.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./assets/goat.jpg"
+          src="https://images.unsplash.com/photo-1516467508483-a7212febe31a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFybSUyMGFuaW1hbHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
           alt="Second slide"
         />
 
         <Carousel.Caption>
           <h3>Animals for sale</h3>
-          <p>Dairy goat for sale.</p>
+          <p>Piglet for sale.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="./assets/sheep.jpeg"
+          src="https://images.unsplash.com/photo-1598612518644-2cf30290e3e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGZhcm0lMjBhbmltYWxzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
           alt="Third slide"
         />
 
         <Carousel.Caption>
           <h3>Animals for sale </h3>
           <p>
-            Woolly sheep for sale.
+            Dairy Goat for sale.
           </p>
         </Carousel.Caption>
       </Carousel.Item>
@@ -54,11 +54,11 @@ const Home = () => {
             height={1000}
           />
           <div className="card-img-overlay d-flex align-items-center">
-            <div className="container" style={{position:'absolute',top:0,left:0}}>
-              <h5 className="card-title fs-1 text fw-bold" style={{color:'black'}}>Welcome to Farmat</h5>
-              <p className="card-text fs-5 d-none d-sm-block " style={{color:'black'}}>
-              Buy farm animals directly from farmers and support their livelihoods
-              </p>
+            <div className="container mt-3" style={{position:'absolute',top:0,left:0}}>
+            <h5 className="card-title fs-1 text fw-bold" style={{ color: 'black', textAlign: 'center', margin: 3 }}>Welcome to Farmat</h5>
+                <p className="card-text fs-5 d-none d-sm-block " style={{ color: 'black', textAlign: 'center', margin: 3 }}>
+                  Buy farm animals directly from farmers and support their livelihoods
+                </p>
             </div>
           </div>
         </div>
