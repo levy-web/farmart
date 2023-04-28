80151e8c30f08f746f9c3c1b057252c67d6024a6
levy <lnjiru43@gmail.com>
2023-04-28 14:20:15 +0300

    changed redirect route

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -16,7 +16,7 @@ const Cart = () => {
         <div className="row">
           <div className="col-md-12 py-5 bg-light text-center">
             <h4 className="p-3 display-5">Your Cart is Empty</h4>
-            <Link to="/" className="btn  btn-outline-dark mx-4">
+            <Link to="/Animal" className="btn  btn-outline-dark mx-4">
               <i className="fa fa-arrow-left"></i> Continue Shopping
             </Link>
           </div>
